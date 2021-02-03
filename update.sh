098060cc7d42102b2876c7db4771559ccec3a443
Rafal <paluch112@gmail.com>
2021-02-03 19:28:13 +0100

    add jedi-vim plugin to update

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-cp -r ./colors ~/.vim_runtime;
-echo 'com! FormatXML :%!python3 -c "import xml.dom.minidom, sys; print(xml.dom.minidom.parse(sys.stdin).toprettyxml())"' >> ~/.vimrc;
-echo 'colors zenburn' >> ~/.vimrc;
+# cp -r ./colors ~/.vim_runtime;
+# echo 'com! FormatXML :%!python3 -c "import xml.dom.minidom, sys; print(xml.dom.minidom.parse(sys.stdin).toprettyxml())"' >> ~/.vimrc;
+# echo 'colors zenburn' >> ~/.vimrc;
+
+git clone --recursive https://github.com/davidhalter/jedi-vim.git ~/.vim_runtime/my_plugins/jedi-vim;
 
 
-echo "Update Vim colors, XML function for xml files - Success"
+echo "Update to my Vim :)"
